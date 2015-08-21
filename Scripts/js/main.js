12fddb45c2999ee37b4fda5b2ddb7d04a6a5fb33
brian lin <brianlin224@gmail.com>
2015-08-21 18:56:24 +0800

    index complete & vote start

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -1,3 +1,11 @@
 $(window).load(function(){
-	
+	var $vedioList = $('.vedio-list');
+
+	$vedioList.find('.vedio-link').each(function(){
+		var $this = $(this),
+			$img  = $this.find('img');
+
+		$img.attr('src', 'http://i.ytimg.com/vi/' + $this.data('id') + '/0.jpg');
+		$img.attr('alt', $this.next().find('.vedio-title').text());
+	});
 });
