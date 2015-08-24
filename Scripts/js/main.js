3eb80117d282a07a597d7073ef85d63618cbad06
brian lin <brianlin224@gmail.com>
2015-08-24 19:08:43 +0800

    merge with ciro`s changing

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -1,29 +1,48 @@
-$(window).load(function(){
-	var $vedioList    = $('.vedio-list'),
+$(document).ready(function(){
+	var $videoList    = $('.video-list'),
+		$videoLink    = $('.video-link'),
 		$btnClose     = $('.btn-close'),
 		$btnSort      = $('.btn-sort'),
 		$lightboxWrap = $('.lightbox-Wrap'),
 		_url          = window.location.href,
-		_id           = _url.split('#')[1];
+		_id           = window.location.hash;
 
-	if (_id !== undefined) {
-		var $target = $('.vedio-link[data-id="' + _id + '"]');
-
-		lightBoxOn(_id, $target.next().find('.vedio-title').text(), $target.next().find('.author').text());
+	if ( _id !== '' ) {
+		for ( var i = 0 ; i < $videoLink.length ; i ++ ) {
+			if ( $videoLink.eq(i).data('id') === _id.split('#')[1] ) {
+				$.fancybox.open({
+					type : 'iframe',
+					iframe : {
+						preload : true
+					},
+					padding : 0,
+					href : $videoLink.eq(i).attr('href'),
+					autoSize : true,
+					afterClose : function() {
+						window.location.hash = '';
+					}
+				});
+			}
+		};
 	}
 
-	$vedioList.find('.vedio-link').each(function(){
-		var $this   = $(this),
-			$img    = $this.find('img'),
-			_title  = $this.next().find('.vedio-title').text(),
-			_author = $this.next().find('.author').text();
+	$videoLink.on('click' , function(e) {
+		var $this = $(this);
+		
+		e.preventDefault();
+		window.location.hash = $this.data('id');
 
-		$img.attr('src', 'http://i.ytimg.com/vi/' + $this.data('id') + '/0.jpg');
-		$img.attr('alt', $this.next().find('.vedio-title').text());
-
-		$this.on('click', function(){
-			lightBoxOn($this.data('id'), _title, _author);
-			window.location.href = _url.split('#')[0] + '#' + $this.data('id');
+		$.fancybox.open({
+			type : 'iframe',
+			iframe : {
+				preload : true
+			},
+			padding : 0,
+			href : $this.attr('href'),
+			autoSize : true,
+			afterClose : function() {
+				window.location.hash = '';
+			}
 		});
 	});
 
@@ -33,28 +52,4 @@ $(window).load(function(){
 			$(this).siblings().removeClass('is-curr');
 		}
 	});
-
-	$btnClose.on('click', function(){
-		$lightboxWrap.removeClass('is-show');
-		window.history.pushState({}, '', _url.split('#')[0]);
-	});
-
-	$lightboxWrap.off('click').on('click', function(e){
-		e.stopPropagation();
-		if (!$(e.target).is('.lightbox, .lightbox *')) {
-			$lightboxWrap.removeClass('is-show');
-			window.history.pushState({}, '', _url.split('#')[0]);
-		}
-	});
-
-	function lightBoxOn(id, title, author) {
-		var _oldID = $('.vedio-frame').attr('src').split('http://www.youtube.com/embed/')[1];
-
-		if (_oldID !== id) {
-			$lightboxWrap.find('.vedio-title').text(title);
-			$lightboxWrap.find('.author').text(author);
-			$('.vedio-frame').attr('src', 'http://www.youtube.com/embed/' + id);
-		}
-		$lightboxWrap.addClass('is-show');
-	}
 });
